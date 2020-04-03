6b28b497da8ad73db1803a526743f637796d98bf
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-04-03 02:14:04 +0000

    feature/user-auth

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,12 @@ Rails.application.routes.draw do
     
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
