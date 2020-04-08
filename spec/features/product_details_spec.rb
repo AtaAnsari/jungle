b31ffc6ca898b400247c908dee8b717eb4dd5975
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-04-08 02:29:07 +0000

    expect(page).to_not have_css '.products-show'

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -23,11 +23,12 @@ RSpec.feature "ProductDetails", type: :feature do
   scenario "They see all products" do
     # ACT
     visit root_path
+    expect(page).to_not have_css '.products-show'
 
     first(:xpath, "//a[@href='/products/1']").click
     puts page.html 
 
     # # VERIFY
-    expect(page).to have_css '.products-show', count: 1
+    expect(page).to have_css '.products-show'
   end
 end
